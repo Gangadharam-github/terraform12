c235c09970c45c51ad9f9e829baeb2cfce8f404e
Gangadharam-github <gangadharamchalla66@gmail.com>
2024-10-13 14:46:45 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ provider "aws" {
 resource "aws_instance" "name" {
   ami="ami-0cc9838aa7ab1dce7"
   instance_type="t2.micro"
-   key_name = "keypairaws"
+   key_name = "God"
    tags = {
      Name="ammullu"
    }
